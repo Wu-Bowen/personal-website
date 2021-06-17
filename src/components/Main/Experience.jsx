58958cdb1b49cffcb216c2ee53a3e9994b578015
Eric Wu <ebw289@nyu.edu>
2021-06-17 14:49:10 -0400

    added data

--- src/components/Main/Experience.jsx
+++ src/components/Main/Experience.jsx
@@ -63,8 +63,6 @@ const Experience = () => {
                 <p> yo</p>
             </div>
         </div>
-
-
     );
 };
 
---
+++ src/components/data/jobData.js
@@ -0,0 +1,45 @@
+const jobData = {
+    companies: [{
+            startDate: '01-20-2020',
+            companyName: 'Bank of America',
+            url: 'https://about.bankofamerica.com/en/our-company',
+            title: 'Software Engineer',
+            location: 'Charlotte, NC',
+            range: 'January 2021 - present',
+            roleDescription: [
+                'Revamped long-standing credit risk platform to use a microservice architecture, substantially reducing load time',
+                'Transitioned the web app’s Spring framework to Node’s Express to maintain continuity between front and backend',
+                'Developed new features including UI/UX changes using React, creation of APIs using Knex and Objection, construction of Oracle Exadata scripts, and contributions to the bank’s reusable component library',
+                'Utilized Jest and React-testing-library for unit testing, RAFT for automated functional testing, and Jenkins for integration testing and deployment',
+                'Worked with container deployment platforms including OpenShift and Ansible'
+            ]
+        },
+        {
+            startDate: '05-10-2019',
+            companyName: 'Pfizer Inc.',
+            url: 'https://www.pfizer.com/',
+            title: 'Software Developer Intern',
+            location: 'New York, NY',
+            range: 'May 2019 - August 2019',
+            roleDescription: [
+                'Enhanced the visualization capabilities for a natural language processing prototype for the Competitive Intelligence Team to empower rapid trend analysis to form business insights',
+                'Drastically improved the rendering capabilities of massive network graphs to be able to handle thousands of nodes from dozens by reducing CPU and memory usage',
+                'Implemented improvements to the backend microservices of the project to further analyze the clinical studies',
+                'Utilized popular JavaScript libraries including React-Redux.js, Node.js, Webpack.js, D3.js, and Sigma.js',
+            ]
+        }, {
+            startDate: '12-1-20',
+            companyName: 'China Unicom',
+            url: 'https://www.chinaunicom.com.hk/en/global/home.php',
+            title: 'Software Engineer Intern',
+            location: 'Shanghai',
+            range: 'December 2018 - January 2019',
+            roleDescription: [
+                'Developed an app that utilized Spring Data JPA to fetch and create APIs to relay potential errors of the surveillance system to a GUI built with JavaFX',
+                'Assisted developers to create a file system for customer database management; responsible for functional and unit testing'
+            ]
+        },
+    ]
+};
+
+export default jobData;
