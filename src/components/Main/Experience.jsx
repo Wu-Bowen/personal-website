61360f939bc74e40b785d54f47d34b8f90f7f122
Eric Wu <ericwu692@yahoo.com>
2021-06-18 09:29:41 -0400

    job titles added

--- src/components/Main/Experience.jsx
+++ src/components/Main/Experience.jsx
@@ -6,7 +6,6 @@ import { KEY_CODES } from './../../utils';
 // import { srConfig } from '@config';
 import sr from './../../utils/sr';
 import configs from './../../config'
-import { useForkRef } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     experienceContainer: {
@@ -16,8 +15,8 @@ const useStyles = makeStyles(theme => ({
         padding: '100px 0 1000px',
         // CHANGE PADDDING
     },
-    tabList:{
-        position: 'relative', 
+    tabList: {
+        position: 'relative',
         zIndex: '3',
         width: 'max-content',
         padding: '0',
@@ -92,7 +91,41 @@ const useStyles = makeStyles(theme => ({
         background: theme.palette.primary.textColor,
         transition: 'transform 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)',
         transitionDelay: '0.1s',
-    }
+    },
+    tabPanels: {
+        position: 'relative',
+        width: '100%',
+        marginLeft: '20px',
+    },
+    tabPanel: {
+        width: '100%',
+        height: 'auto',
+        padding: '10px 5px',
+    },
+    h3: {
+        marginBottom: '2px',
+        fontSize: '22px',
+        fontWeight: '500',
+        lineHeight: '1.3',
+        color: theme.palette.secondary.main
+    },
+    anchor: {
+        display: 'inline-block',
+        textDecoration: 'none',
+        textDecorationColor: 'transparent',
+        color: theme.palette.primary.textColor,
+        '&::after': {
+            content: '""',
+            width: '0px',
+            height: '2px',
+            display: 'block',
+            background: theme.palette.primary.textColorLower,
+            transition: '300ms',
+        },
+        '&:hover::after': {
+            width: '100%',
+        }
+    },
 }));
 const Experience = () => {
     const classes = useStyles();
@@ -146,31 +179,64 @@ const Experience = () => {
                 Where I've Worked
             </h2>
             <div className={classes.inner}>
-                <div className= {classes.tabList} role="tablist" aria-label="Job tabs" onKeyDown={(e) => { onKeyDown(e) }}>
+                <div className={classes.tabList} role="tablist" aria-label="Job tabs" onKeyDown={(e) => { onKeyDown(e) }}>
                     {jobData?.companies && jobData.companies.map((company, i) => {
-                        return <button
-                            key={i}
-                            className={classes.tabButton}
-                            style={currentTab === i ? ({
-                                color: '#77ddaa',
-                                cursor: 'pointer',
-                            }
-                            ) : ({
-                                color: '#8892b0',
-                                cursor: 'pointer',
-                            })}
-                            onClick={() => { setCurrentTab(i) }}
-                            ref={el => (tabs.current[i] = el)}
-                            id={`tab-${i}`}
-                            role="tab"
-                            tabIndex={currentTab === i ? '0' : '-1'}
-                            aria-selected={currentTab === i ? '0' : '1'}
-                            aria-controls={`panel-${i}`}
-                        >
-                            <span> {company.companyName} </span>
-                        </button>
+                        return (
+                            <button
+                                key={i}
+                                className={classes.tabButton}
+                                style={currentTab === i ? ({
+                                    color: '#77ddaa',
+                                    cursor: 'pointer',
+                                }
+                                ) : ({
+                                    color: '#8892b0',
+                                    cursor: 'pointer',
+                                })}
+                                onClick={() => { setCurrentTab(i) }}
+                                ref={el => (tabs.current[i] = el)}
+                                id={`tab-${i}`}
+                                role="tab"
+                                tabIndex={currentTab === i ? '0' : '-1'}
+                                aria-selected={currentTab === i ? '0' : '1'}
+                                aria-controls={`panel-${i}`}
+                            >
+                                <span> {company.companyName} </span>
+                            </button>
+                        )
                     })}
-                    <div className={classes.activeTab} style={{transform: `translateY(${currentTab * 42}px)`}}/>
+                    <div className={classes.activeTab} style={{ transform: `translateY(${currentTab * 42}px)` }} />
+                </div>
+
+
+                <div className={classes.tabPanels}>
+                    {jobData?.companies && jobData.companies.map((company, i) => {
+                        return (
+                            <div
+                                className={classes.tabPanel}
+                                id={`panel-${i}`}
+                                role="tabpanel"
+                                tabIndex={currentTab === i ? '0' : '-1'}
+                                aria-labelledby={`tab-${i}`}
+                                aria-hidden={currentTab !== i}
+                                hidden={currentTab !== i}
+                            >
+                                <h3 className={classes.h3}>
+                                    <span> {company.title} </span>
+                                    <span className="company">
+                                        {' @ '}
+                                        <a href={company.url} className={classes.anchor} rel="noopener noreferrer" target="_blank">
+                                            {company.companyName}
+                                        </a>
+                                    </span>
+                                </h3>
+
+
+
+                            </div>
+
+                        )
+                    })}
                 </div>
             </div>
         </div>
