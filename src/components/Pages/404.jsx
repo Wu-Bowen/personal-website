6bb57668c1926d6600206657ff7db26ef149ceb4
Eric Wu <ericwu692@yahoo.com>
2022-02-05 09:06:14 -0500

    hotdog

--- src/components/Pages/404.jsx
+++ src/components/Pages/404.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import makeStyles from '@mui/styles/makeStyles';
 import { Link } from 'react-router-dom';
+import Hotdog from './../data/images/hotdog.gif';
 
 const useStyles = makeStyles((theme) => ({
     notFound: {
-        marginTop: '10vh',
+        marginTop: '9vh',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
         flexDirection: 'column',
         height: 'auto',
         minHeight: '64px',
+        color: theme.palette.secondary.textColor,
+        fontFamily: '"IBM Plex Serif",Big Caslon,serif',
+        backgroundImage: "url(" + Hotdog + ")",
+        backgroundSize: 'cover',
+        height: '100vh',
+    },
+    text: {
+        maxWidth: '30vw',
+        paddingLeft: '10vw',
+    },
+    title: {
+        fontSize: 'clamp(50px,25vh,200px)',
+        margin: 0,
     },
 }));
 const NotFoundPage = () => {
     const classes = useStyles();
     return (
         <div className={classes.notFound}>
-            <p>
-                <Link to="/">Go to Home </Link>
-            </p>
+            <div className={classes.text}>
+                <p className={classes.title}> 404 </p>
+                <p> No es bueno </p>
+                <p>
+                    You came here, looking for something, and all you get is
+                    this silly running hot dog. Not good. Not good, at all.
+                </p>
+                <p>
+                    
+                    <Link to="/">Back To Safety</Link>
+                </p>
+            </div>
         </div>
     );
 };
